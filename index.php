3e3e603765726267bad87eeeb0749e341bc5d46b
吉田泰樹 <yoshidataiga@mbp.local>
2020-05-05 17:35:54 +0900

    ver 2

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>FizzBuzz Game</title>
+</head>
+<body>
+  <form id="number" method="post">
+    <p>fizzNum<input type="text" name="fizz"></p>
+    <p>buzzNum<input type="text" name="buzz"></p>
+    <input type="submit" name="submit" value="実行">
+  </form>
+  出力
+  <?php 
+
+  if($_POST['submit']) {
+    $fizz = ($_POST['fizz']);
+    $buzz = ($_POST['buzz']);
+    if(ctype_digit($fizz) && ctype_digit($buzz) ) {
+        for($i=1; $i < 100; $i++) {
+          if($i % $fizz === 0 && $i % $buzz === 0) {
+            echo '<br>' . $i . 'FizzBuzz';
+          } elseif($i % $fizz === 0) {
+            echo '<br>' . $i . 'Fizz';
+          } elseif($i % $buzz === 0) {
+            echo '<br>' . $i . 'Buzz';
+          }
+      } 
+    } else {
+       echo '整数を入力して下さい';
+    }
+  }
+  ?>
+</body>
+</html>
