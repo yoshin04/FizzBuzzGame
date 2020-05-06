46157d831d7ddddc5b2c9fd5131d2b4c153e81cf
吉田泰樹 <yoshidataiga@mbp.local>
2020-05-06 16:26:45 +0900

    正規表現とキャストを追加

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,17 +16,21 @@
   <?php 
 
   if($_POST['submit']) {
-    $fizz = ($_POST['fizz']);
-    $buzz = ($_POST['buzz']);
-    if(ctype_digit($fizz) && ctype_digit($buzz) ) {
-        for($i=1; $i < 100; $i++) {
-          if($i % $fizz === 0 && $i % $buzz === 0) {
-            echo '<br>' . $i . 'FizzBuzz';
-          } elseif($i % $fizz === 0) {
+    $fizz = $_POST['fizz'];
+    $buzz = $_POST['buzz'];
+    $match = "/^[0-9]+$/";
+    echo preg_match($a, $fizz);
+    if (preg_match($match, $fizz) === 1 && preg_match($match, $buzz) === 1) {
+      $fizz = (int)$fizz;
+      $buzz = (int)$buzz;
+      for($i=1; $i < 100; $i++) {
+        if($i % $fizz === 0 && $i % $buzz === 0) {
+          echo '<br>' . $i . 'FizzBuzz';
+        } elseif($i % $fizz === 0) {
             echo '<br>' . $i . 'Fizz';
           } elseif($i % $buzz === 0) {
-            echo '<br>' . $i . 'Buzz';
-          }
+              echo '<br>' . $i . 'Buzz';
+            }
       } 
     } else {
        echo '整数を入力して下さい';
